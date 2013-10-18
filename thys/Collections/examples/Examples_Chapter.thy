11879a7f777b66d846859e4f4f027699e47a1817
lammich <lammich@in.tum.de>
2013-10-18 16:51:28 +0200

    Adapted Refine_Monadic, Collections, and dependent thys to use new Automatic_Refinement entry

--- thys/Collections/examples/Examples_Chapter.thy
+++ thys/Collections/examples/Examples_Chapter.thy
@@ -1,4 +1,12 @@
-header {* \chapter{Examples} \label{ch:examples} *}
 (*<*)
-theory Examples_Chapter imports Main begin end
+theory Examples_Chapter imports Main begin 
+(*>*)
+text_raw {* \isachapter{Examples} *}
+text {*
+  This chapter contains various usage examples.
+  The examples vary from small snippets to fully-fledged algorithm 
+  developments.
+*}
+(*<*)
+end
 (*>*)
