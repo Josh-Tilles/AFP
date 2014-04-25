fdb70430b5e1f67e720ced56c08877b19db829c9
Josh Tilles <MerelyAPseudonym@gmail.com>
2014-04-24 22:08:20 -0400

    Tweak formatting; use Isar-style declarations.

--- thys/Category3/Category.thy
+++ thys/Category3/Category.thy
@@ -2,7 +2,7 @@ theory Category
 imports "~~/src/HOL/Library/FuncSet"
 begin
 
-record ('o,'m) Category = 
+record ('o, 'm) Category =
   Obj :: "'o set" ("obj\<index>" 70) 
   Mor :: "'m set" ("mor\<index>" 70)
   Dom :: "'m \<Rightarrow> 'o" ("dom\<index> _" [80] 70)
@@ -11,48 +11,54 @@ record ('o,'m) Category =
   Comp :: "'m \<Rightarrow> 'm \<Rightarrow> 'm" (infixl ";;\<index>" 70)
 
 definition
-  MapsTo :: "('o,'m,'a) Category_scheme \<Rightarrow> 'm \<Rightarrow> 'o \<Rightarrow> 'o \<Rightarrow> bool" ("_ maps\<index> _ to _" [60, 60, 60] 65) where
+  MapsTo :: "('o, 'm, 'a) Category_scheme \<Rightarrow> 'm \<Rightarrow> 'o \<Rightarrow> 'o \<Rightarrow> bool" ("_ maps\<index> _ to _" [60, 60, 60] 65) where
   "MapsTo CC f X Y \<equiv> f \<in> Mor CC \<and> Dom CC f = X \<and> Cod CC f = Y"
 
 definition
-  CompDefined :: "('o,'m,'a) Category_scheme \<Rightarrow> 'm \<Rightarrow> 'm \<Rightarrow> bool" (infixl "\<approx>>\<index>" 65) where
+  CompDefined :: "('o, 'm, 'a) Category_scheme \<Rightarrow> 'm \<Rightarrow> 'm \<Rightarrow> bool" (infixl "\<approx>>\<index>" 65) where
   "CompDefined CC f g \<equiv> f \<in> Mor CC \<and> g \<in> Mor CC \<and> Cod CC f = Dom CC g"
 
-locale ExtCategory = 
-  fixes C :: "('o,'m,'a) Category_scheme" (structure)
+locale ExtCategory =
+  fixes C :: "('o, 'm, 'a) Category_scheme" (structure)
   assumes CdomExt: "(Dom C) \<in> extensional (Mor C)"
-  and     CcodExt: "(Cod C) \<in> extensional (Mor C)"
-  and     CidExt:  "(Id C) \<in> extensional (Obj C)"
-  and     CcompExt:  "(split (Comp C)) \<in> extensional ({(f,g) | f g . f \<approx>> g})"
+      and CcodExt: "(Cod C) \<in> extensional (Mor C)"
+      and CidExt:  "(Id C) \<in> extensional (Obj C)"
+      and CcompExt: "(split (Comp C)) \<in> extensional { (f, g) | f g . f \<approx>> g}"
 
 locale Category = ExtCategory +
-  assumes Cdom : "f \<in> mor \<Longrightarrow> dom f \<in> obj"
-  and     Ccod : "f \<in> mor \<Longrightarrow> cod f \<in> obj"
-  and     Cidm [dest]: "X \<in> obj \<Longrightarrow> (id X) maps X to X"
-  and     Cidl : "f \<in> mor \<Longrightarrow> id (dom f) ;; f = f"
-  and     Cidr : "f \<in> mor \<Longrightarrow> f ;; id (cod f) = f"
-  and     Cassoc : "\<lbrakk>f \<approx>> g ; g \<approx>> h\<rbrakk> \<Longrightarrow> (f ;; g) ;; h = f ;; (g ;; h)"
-  and     Ccompt : "\<lbrakk>f maps X to Y ; g maps Y to Z\<rbrakk> \<Longrightarrow> (f ;; g) maps X to Z"
+  assumes Cdom: "f \<in> mor \<Longrightarrow> dom f \<in> obj"
+      and Ccod: "f \<in> mor \<Longrightarrow> cod f \<in> obj"
+      and Cidm [dest]: "X \<in> obj \<Longrightarrow> (id X) maps X to X"
+      and Cidl: "f \<in> mor \<Longrightarrow> id (dom f) ;; f = f"
+      and Cidr: "f \<in> mor \<Longrightarrow> f ;; id (cod f) = f"
+      and Cassoc: "\<lbrakk>f \<approx>> g; g \<approx>> h\<rbrakk> \<Longrightarrow> (f ;; g) ;; h = f ;; (g ;; h)"
+      and Ccompt: "\<lbrakk>f maps X to Y; g maps Y to Z\<rbrakk> \<Longrightarrow> (f ;; g) maps X to Z"
 
 definition 
-  MakeCat :: "('o,'m,'a) Category_scheme \<Rightarrow> ('o,'m,'a) Category_scheme" where
+  MakeCat :: "('o, 'm, 'a) Category_scheme \<Rightarrow> ('o, 'm, 'a) Category_scheme" where
   "MakeCat C \<equiv> \<lparr>
       Obj = Obj C , 
       Mor = Mor C , 
       Dom = restrict (Dom C) (Mor C) , 
       Cod = restrict (Cod C) (Mor C) , 
       Id  = restrict (Id C) (Obj C) , 
-      Comp = \<lambda> f g . (restrict (split (Comp C)) ({(f,g) | f g . f \<approx>>\<^bsub>C\<^esub> g})) (f,g), 
+      Comp = \<lambda> f g . (restrict (split (Comp C)) {(f,g) | f g . f \<approx>>\<^bsub>C\<^esub> g})(f, g), 
       \<dots> = Category.more C
   \<rparr>"
 
-lemma MakeCatMapsTo: "f maps\<^bsub>C\<^esub> X to Y \<Longrightarrow> f maps\<^bsub>MakeCat C\<^esub> X to Y"
+lemma MakeCatMapsTo:
+  assumes "f maps\<^bsub>C\<^esub> X to Y"
+  shows "f maps\<^bsub>MakeCat C\<^esub> X to Y"
 sorry
 
-lemma MakeCatComp: "f \<approx>>\<^bsub>C\<^esub> g \<Longrightarrow> f ;;\<^bsub>MakeCat C\<^esub> g = f ;;\<^bsub>C\<^esub> g"
+lemma MakeCatComp:
+  assumes "f \<approx>>\<^bsub>C\<^esub> g"
+  shows "f ;;\<^bsub>MakeCat C\<^esub> g = f ;;\<^bsub>C\<^esub> g"
 sorry
 
-lemma MakeCatId: "X \<in> obj\<^bsub>C\<^esub> \<Longrightarrow> id\<^bsub>C\<^esub> X = id\<^bsub>MakeCat C\<^esub> X"
+lemma MakeCatId:
+  assumes "X \<in> obj\<^bsub>C\<^esub>"
+  shows "id\<^bsub>C\<^esub> X = id\<^bsub>MakeCat C\<^esub> X"
 sorry
 
 lemma MakeCatObj: "obj\<^bsub>MakeCat C\<^esub> = obj\<^bsub>C\<^esub>"
@@ -61,40 +67,64 @@ sorry
 lemma MakeCatMor: "mor\<^bsub>MakeCat C\<^esub> = mor\<^bsub>C\<^esub>"
 sorry
 
-lemma MakeCatDom: "f \<in> mor\<^bsub>C\<^esub> \<Longrightarrow> dom\<^bsub>C\<^esub> f = dom\<^bsub>MakeCat C\<^esub> f"
+lemma MakeCatDom:
+  assumes "f \<in> mor\<^bsub>C\<^esub>"
+  shows "dom\<^bsub>C\<^esub> f = dom\<^bsub>MakeCat C\<^esub> f"
 sorry
 
-lemma MakeCatCod: "f \<in> mor\<^bsub>C\<^esub> \<Longrightarrow> cod\<^bsub>C\<^esub> f = cod\<^bsub>MakeCat C\<^esub> f"
+lemma MakeCatCod:
+  assumes "f \<in> mor\<^bsub>C\<^esub>"
+  shows "cod\<^bsub>C\<^esub> f = cod\<^bsub>MakeCat C\<^esub> f"
 sorry
 
 
-lemma MakeCatCompDef: "f \<approx>>\<^bsub>MakeCat C\<^esub> g = f \<approx>>\<^bsub>C\<^esub> g"
+lemma MakeCatCompDef: "f \<approx>>\<^bsub>MakeCat C\<^esub> g \<longleftrightarrow> f \<approx>>\<^bsub>C\<^esub> g"
 sorry
 
-lemma MakeCatComp2: "f \<approx>>\<^bsub>MakeCat C\<^esub> g \<Longrightarrow> f ;;\<^bsub>MakeCat C\<^esub> g = f ;;\<^bsub>C\<^esub> g"
+lemma MakeCatComp2:
+  assumes "f \<approx>>\<^bsub>MakeCat C\<^esub> g"
+  shows "f ;;\<^bsub>MakeCat C\<^esub> g = f ;;\<^bsub>C\<^esub> g"
 sorry
 
 
 lemma ExtCategoryMakeCat: "ExtCategory (MakeCat C)"
 sorry
 
-lemma MakeCat: "Category_axioms C \<Longrightarrow> Category (MakeCat C)"
+lemma MakeCat:
+  assumes "Category_axioms C"
+  shows "Category (MakeCat C)"
 sorry
 
 
-lemma MapsToE[elim]: "\<lbrakk>f maps\<^bsub>C\<^esub> X to Y ; \<lbrakk>f \<in> mor\<^bsub>C\<^esub> ; dom\<^bsub>C\<^esub> f = X ; cod\<^bsub>C\<^esub> f = Y\<rbrakk> \<Longrightarrow> R\<rbrakk> \<Longrightarrow> R"
+lemma MapsToE [elim]:
+  assumes "f maps\<^bsub>C\<^esub> X to Y"
+    and "\<lbrakk>f \<in> mor\<^bsub>C\<^esub>; dom\<^bsub>C\<^esub> f = X; cod\<^bsub>C\<^esub> f = Y\<rbrakk> \<Longrightarrow> R"
+  shows "R"
 sorry
 
-lemma MapsToI[intro]: "\<lbrakk>f \<in> mor\<^bsub>C\<^esub> ; dom\<^bsub>C\<^esub> f = X ; cod\<^bsub>C\<^esub> f = Y\<rbrakk> \<Longrightarrow> f maps\<^bsub>C\<^esub> X to Y"
+lemma MapsToI [intro]:
+  assumes "f \<in> mor\<^bsub>C\<^esub>"
+    and "dom\<^bsub>C\<^esub> f = X"
+    and "cod\<^bsub>C\<^esub> f = Y"
+  shows "f maps\<^bsub>C\<^esub> X to Y"
 sorry
 
-lemma CompDefinedE[elim]: "\<lbrakk>f \<approx>>\<^bsub>C\<^esub> g ; \<lbrakk>f \<in> mor\<^bsub>C\<^esub> ; g \<in> mor\<^bsub>C\<^esub> ; cod\<^bsub>C\<^esub> f = dom\<^bsub>C\<^esub> g\<rbrakk> \<Longrightarrow> R\<rbrakk> \<Longrightarrow> R"
+lemma CompDefinedE [elim]:
+  assumes "f \<approx>>\<^bsub>C\<^esub> g"
+      and "\<lbrakk>f \<in> mor\<^bsub>C\<^esub> ; g \<in> mor\<^bsub>C\<^esub> ; cod\<^bsub>C\<^esub> f = dom\<^bsub>C\<^esub> g\<rbrakk> \<Longrightarrow> R"
+  shows "R"
 sorry
 
-lemma CompDefinedI[intro]: "\<lbrakk>f \<in> mor\<^bsub>C\<^esub> ; g \<in> mor\<^bsub>C\<^esub> ; cod\<^bsub>C\<^esub> f = dom\<^bsub>C\<^esub> g\<rbrakk> \<Longrightarrow> f \<approx>>\<^bsub>C\<^esub> g"
+lemma CompDefinedI [intro]:
+  assumes "f \<in> mor\<^bsub>C\<^esub>"
+    and "g \<in> mor\<^bsub>C\<^esub>"
+    and "cod\<^bsub>C\<^esub> f = dom\<^bsub>C\<^esub> g"
+  shows "f \<approx>>\<^bsub>C\<^esub> g"
 sorry
 
-lemma (in Category) MapsToCompI: assumes "f \<approx>> g" shows "(f ;; g) maps (dom f) to (cod g)"
+lemma (in Category) MapsToCompI:
+  assumes "f \<approx>> g"
+  shows "(f ;; g) maps (dom f) to (cod g)"
 sorry
 
 
@@ -106,7 +136,7 @@ sorry
 
 lemma (in Category) MapsToMorDomCod: 
   assumes "f \<approx>> g" 
-  shows "f ;; g \<in> mor" and "dom (f ;; g) = dom f" and "cod (f ;; g) = cod g"
+  shows "f;;g \<in> mor" and "dom (f;;g) = dom f" and "cod (f;;g) = cod g"
 sorry
 
 
@@ -128,10 +158,14 @@ lemma (in Category) CompDefComp:
 sorry
 
 
-lemma (in Category) CatIdInMor: "X \<in> obj \<Longrightarrow> id X \<in> mor"
+lemma (in Category) CatIdInMor:
+  assumes "X \<in> obj"
+  shows "id X \<in> mor"
 sorry
 
-lemma (in Category) MapsToId: assumes "X \<in> obj" shows "id X \<approx>> id X"
+lemma (in Category) MapsToId:
+  assumes "X \<in> obj"
+  shows "id X \<approx>> id X"
 sorry
 
 
@@ -167,17 +201,24 @@ sorry
 
   
 definition
-  inverse_rel :: "('o,'m,'a) Category_scheme \<Rightarrow> 'm \<Rightarrow> 'm \<Rightarrow> bool" ("cinv\<index> _ _" 60) where
+  inverse_rel :: "('o, 'm, 'a) Category_scheme \<Rightarrow> 'm \<Rightarrow> 'm \<Rightarrow> bool" ("cinv\<index> _ _" 60) where
   "inverse_rel C f g \<equiv> (f \<approx>>\<^bsub>C\<^esub> g) \<and> (f ;;\<^bsub>C\<^esub> g) = (id\<^bsub>C\<^esub> (dom\<^bsub>C\<^esub> f)) \<and> (g ;;\<^bsub>C\<^esub> f) = (id\<^bsub>C\<^esub> (cod\<^bsub>C\<^esub> f))"
 
 definition 
-  isomorphism :: "('o,'m,'a) Category_scheme \<Rightarrow> 'm \<Rightarrow> bool" ("ciso\<index> _" [70]) where
+  isomorphism :: "('o, 'm, 'a) Category_scheme \<Rightarrow> 'm \<Rightarrow> bool" ("ciso\<index> _" [70]) where
   "isomorphism C f \<equiv> \<exists> g . inverse_rel C f g"
 
-lemma (in Category) Inverse_relI: "\<lbrakk>f \<approx>> g ; f ;; g = id (dom f) ; g ;; f = id (cod f)\<rbrakk> \<Longrightarrow> (cinv f g)"
+lemma (in Category) Inverse_relI:
+  assumes "f \<approx>> g"
+      and "f ;; g = id (dom f)"
+      and "g ;; f = id (cod f)"
+  shows "cinv f g"
 sorry
 
-lemma (in Category) Inverse_relE[elim]: "\<lbrakk>cinv f g ; \<lbrakk>f \<approx>> g ; f ;; g = id (dom f) ; g ;; f = id (cod f)\<rbrakk> \<Longrightarrow> P\<rbrakk> \<Longrightarrow> P"
+lemma (in Category) Inverse_relE [elim]:
+  assumes "cinv f g"
+      and "\<lbrakk>f \<approx>> g; f ;; g = id (dom f); g ;; f = id (cod f)\<rbrakk> \<Longrightarrow> P"
+  shows "P"
 sorry
 
 lemma (in Category) Inverse_relSym: 
@@ -193,12 +234,14 @@ lemma (in Category) InverseUnique:
 sorry
 
 
-lemma (in Category) InvId: assumes "X \<in> obj" shows "(cinv (id X) (id X))"
+lemma (in Category) InvId:
+  assumes "X \<in> obj"
+  shows "cinv (id X) (id X)"
 sorry
 
     
 definition
-  inverse :: "('o,'m,'a) Category_scheme \<Rightarrow> 'm \<Rightarrow> 'm" ("Cinv\<index> _" [70]) where
+  inverse :: "('o, 'm, 'a) Category_scheme \<Rightarrow> 'm \<Rightarrow> 'm" ("Cinv\<index> _" [70]) where
   "inverse C f \<equiv> THE g . inverse_rel C f g"
 
 lemma (in Category) inv2Inv:
@@ -219,10 +262,14 @@ lemma (in Category) InvInv:
 sorry
 
 
-lemma (in Category) InvIsMor: "(cinv f g) \<Longrightarrow> (f \<in> mor \<and> g \<in> mor)"
+lemma (in Category) InvIsMor:
+  assumes "cinv f g"
+  shows "f \<in> mor \<and> g \<in> mor"
 sorry
 
-lemma (in Category) IsoIsMor: "ciso f \<Longrightarrow> f \<in> mor"
+lemma (in Category) IsoIsMor:
+  assumes "ciso f"
+  shows "f \<in> mor"
 sorry
 
 lemma (in Category) InvDomCod:
@@ -231,16 +278,24 @@ lemma (in Category) InvDomCod:
 sorry
 
 
-lemma (in Category) IsoCompInv: "ciso f \<Longrightarrow> f \<approx>> Cinv f"
+lemma (in Category) IsoCompInv:
+  assumes "ciso f"
+  shows "f \<approx>> Cinv f"
 sorry
 
-lemma (in Category) InvCompIso: "ciso f \<Longrightarrow> Cinv f \<approx>> f"
+lemma (in Category) InvCompIso:
+  assumes "ciso f"
+  shows "Cinv f \<approx>> f"
 sorry
 
-lemma (in Category) IsoInvId1 : "ciso f \<Longrightarrow> (Cinv f) ;; f = (id (cod f))"
+lemma (in Category) IsoInvId1:
+  assumes "ciso f"
+  shows "(Cinv f) ;; f = (id (cod f))"
 sorry
 
-lemma (in Category) IsoInvId2 :  "ciso f \<Longrightarrow> f ;; (Cinv f) = (id (dom f))"
+lemma (in Category) IsoInvId2:
+  assumes "ciso f"
+  shows "f ;; (Cinv f) = (id (dom f))"
 sorry
 
 lemma (in Category) IsoCompDef:
@@ -260,20 +315,20 @@ definition "ObjIso C A B \<equiv> \<exists> k . (k maps\<^bsub>C\<^esub> A to B)
 definition 
   UnitCategory :: "(unit, unit) Category" where
   "UnitCategory = MakeCat \<lparr>
-      Obj = {()} , 
-      Mor = {()} , 
-      Dom = (\<lambda>f.()) , 
-      Cod = (\<lambda>f.()) , 
-      Id = (\<lambda>f.()) , 
-      Comp = (\<lambda>f g. ())
+      Obj = { () } ,
+      Mor = { () } ,
+      Dom = (\<lambda>f. () ) ,
+      Cod = (\<lambda>f. () ) ,
+      Id = (\<lambda>f. () ) ,
+      Comp = (\<lambda>f g. () )
   \<rparr>"
 
-lemma [simp]: "Category(UnitCategory)"
+lemma [simp]: "Category UnitCategory"
 sorry
 
 
 definition 
-  OppositeCategory :: "('o,'m,'a) Category_scheme \<Rightarrow> ('o,'m,'a) Category_scheme" ("Op _" [65] 65) where
+  OppositeCategory :: "('o, 'm, 'a) Category_scheme \<Rightarrow> ('o, 'm, 'a) Category_scheme" ("Op _" [65] 65) where
   "OppositeCategory C \<equiv> \<lparr>
       Obj = Obj C , 
       Mor = Mor C , 
@@ -288,23 +343,35 @@ lemma OpCatOpCat: "Op (Op C) = C"
 sorry
 
 
-lemma OpCatCatAx: "Category_axioms C \<Longrightarrow> Category_axioms (Op C)"
+lemma OpCatCatAx:
+  assumes "Category_axioms C"
+  shows "Category_axioms (Op C)"
 sorry
 
-lemma OpCatCatExt: "ExtCategory C \<Longrightarrow> ExtCategory (Op C)"
+lemma OpCatCatExt:
+  assumes "ExtCategory C"
+  shows "ExtCategory (Op C)"
 sorry
 
-lemma OpCatCat: "Category C \<Longrightarrow> Category (Op C)"
+lemma OpCatCat:
+  assumes "Category C"
+  shows "Category (Op C)"
 sorry
 
 
-lemma MapsToOp: "f maps\<^bsub>C \<^esub>X to Y \<Longrightarrow> f maps\<^bsub>Op C \<^esub>Y to X"
+lemma MapsToOp:
+  assumes "f maps\<^bsub>C\<^esub> X to Y"
+  shows "f maps\<^bsub>Op C\<^esub> Y to X"
 sorry
 
-lemma MapsToOpOp: "f maps\<^bsub>Op C \<^esub>X to Y \<Longrightarrow> f maps\<^bsub>C \<^esub>Y to X"
+lemma MapsToOpOp:
+  assumes "f maps\<^bsub>Op C\<^esub>X to Y"
+  shows "f maps\<^bsub>C\<^esub> Y to X"
 sorry
 
-lemma CompDefOp: "f \<approx>>\<^bsub>C\<^esub> g \<Longrightarrow> g \<approx>>\<^bsub>Op C\<^esub> f"
+lemma CompDefOp:
+  assumes "f \<approx>>\<^bsub>C\<^esub> g"
+  shows "g \<approx>>\<^bsub>Op C\<^esub> f"
 sorry
 
 end
